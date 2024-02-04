106dc7abb29864d8fa716b43330c6d72f87eb743
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-02-04 23:48:18 +0100

    Finalize first function for main data

--- src/ticker.py
+++ src/ticker.py
@@ -2,9 +2,11 @@
 Moduel for the Ticker class
 """
 
+import httpx
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import httpx
+
 
 class Ticker:
     """
@@ -14,29 +16,43 @@ class Ticker:
     def __init__(self, symbol):
         self.symbol = symbol
 
-    def get_data(self):
+    def get_data(self) -> pd.DataFrame:
         """
         Get data for the ticker
-        """
 
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the data
+        visible in the Yahoo Finance first page for the ticker
+        """
 
         # URL of the website to scrape
         url = "https://finance.yahoo.com/quote/AAPL"
 
         # Send an HTTP GET request to the website
-        response = httpx.get(url)
+        # response = httpx.get(url)
+        response = requests.get(url)
 
         # If the HTTP GET request can't be served
         if response.status_code != 200:
             raise Exception("Failed to load page")
-        
+
         # Parse the HTML content of the website
-        soup = BeautifulSoup(response.content, 'html.parser')
-        print(soup.prettify())
-        # write to a file
-        with open('soup.html', 'w') as file:
-            file.write(str(soup.prettify()))
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # for data in the table, generating 16 rows
+        raw_data = soup.find_all("td", {"data-test": True})
+        data_df = pd.DataFrame()
+        for item in raw_data:
+            data_df[item["data-test"].replace("-value", "")] = [item.text]
+
+        # for data in top of the page, generating 10 rows
+        raw_data = soup.find_all("fin-streamer", {"data-field": True})
+        for item in raw_data:
+            data_df[item["data-field"]] = [item.text]
+
+        return data_df.T
+
 
 if __name__ == "__main__":
-    ticker = Ticker('AAPL')
+    ticker = Ticker("AAPL")
     ticker.get_data()
