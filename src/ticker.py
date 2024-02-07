89075ae471566a9b8c9ceaea5c38fcd65cde5581
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-02-07 23:32:13 +0100

    Add cash flow and analysis methods

--- src/ticker.py
+++ src/ticker.py
@@ -169,10 +169,86 @@ class Ticker:
             data_df[criteria] = data_list
 
         return data_df.T
-        
+
+    def cash_flow(self) -> pd.DataFrame:
+        """
+        Get cash flow for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the cash flow
+        visible in the Yahoo Finance statistics page for the ticker
+        """
+
+        # URL of the website to scrape
+        url = f"https://finance.yahoo.com/quote/{self.ticker}/cash-flow"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+        response = self.get_response(url, headers)
+
+        # Parse the HTML content of the website
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # <div class="" data-test="fin-row">
+        raw_data = soup.find_all("div", {"data-test": "fin-row"})
+
+        data_df = pd.DataFrame()
+        for item in raw_data:
+            # get criteria. e.g. "Total Revenue"
+            major_div = item.find_all("div", {"class": True})[0]
+            criteria = major_div.find_all("div", {"class": True})[0].find("span").text
+
+            # get data. e.g. "274515000000"
+            minor_div = major_div.find_all("div", {"class": "Ta(c)"})
+            data_list = []
+            for div in minor_div:
+                data_list.append(div.text)
+
+            data_df[criteria] = data_list
+
+        return data_df.T
+
+    def analysis(self) -> pd.DataFrame:
+        """
+        Get analysis for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the analysis
+        visible in the Yahoo Finance statistics page for the ticker
+        """
+
+        # URL of the website to scrape
+        url = f"https://finance.yahoo.com/quote/{self.ticker}/analysis"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+        response = self.get_response(url, headers)
+
+        # Parse the HTML content of the website
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        raw_data = soup.find_all("tbody")
+
+        data_df = pd.DataFrame()
+        for item in raw_data:
+            for row in item.find_all("tr"):
+                row = row.find_all("td")
+                criteria = row[0].text
+
+                # the rest of the row is the data
+                data_list = []
+                for data in row[1:]:
+                    data_list.append(data.text)
+
+                data_df[criteria] = data_list
+
+        return data_df.T
+
 if __name__ == "__main__":
     ticker = Ticker("AAPL")
     # ticker.summary()
     # ticker.statistics()
-    ticker.income_stmt()
-    ticker.balance_sheet()
+    # ticker.income_stmt()
+    # ticker.balance_sheet()
+    # ticker.cash_flow()
+    ticker.analysis()
