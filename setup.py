65e3e56363ad413ea2a64c20fdc49c600abd4a11
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-02-08 19:08:51 +0100

    Add version number to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import find_packages, setup
 
+from __version__ import version
+
 setup(
     name="stockdex",
-    version="0.1.2",
+    version=version,
     packages=find_packages(),
-    install_requires=["pandas==2.0.3", "beautifulsoup4==4.12.2"],
+    install_requires=open("requirements.txt").read().splitlines(),
     python_requires=">=3.8",
     author="Amir Nazary",
     description="A package to get stock data from Yahoo Finance",
