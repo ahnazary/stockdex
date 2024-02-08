14842a06d48d112a2a33b3d4302998765a3c2952
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-02-08 01:30:17 +0100

    Add stockex package and tests

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stockex',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas==2.0.3',
+        'beautifulsoup4==4.12.2'
+    ],
+    python_requires='>=3.8',
+    author='Amir Nazary',
+    description='A package to get stock data from Yahoo Finance',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ahnazary/StockDataExtractor',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
