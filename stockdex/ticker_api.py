8ef772c9a3f0535f8a8388ccf7263204a735181a
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-02-24 20:19:04 +0100

    Fix formatting in readme and add nasdaq_interface and ticker_base modules

--- stockdex/ticker_api.py
+++ stockdex/ticker_api.py
@@ -39,10 +39,10 @@ class TickerAPI(TickerBase):
 
         Args:
         range (str): The range of the chart data to retrieve
-            valid values are "1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"
+        valid values are "1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"
 
         dataGranularity (str): The granularity of the data to retrieve (interval)
-            valid values are "1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo""  # noqa: E501
+        valid values are "1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo""  # noqa: E501
         """
 
         url = f"{self.base_url}/chart/{self.ticker}?range={range}&interval={dataGranularity}"
