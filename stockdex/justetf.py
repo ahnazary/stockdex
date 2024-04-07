712f5c1ac020eb2cbf241f5c71af42034dc0fa3b
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-04-07 11:00:34 +0200

    Add new methods for retrieving basic information and holdings of an ETF

--- stockdex/justetf.py
+++ stockdex/justetf.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 
 from stockdex import config
 from stockdex.exceptions import NoISINError
+from stockdex.selenium_interface import selenium_interface
 from stockdex.ticker_base import TickerBase
 
 
@@ -76,3 +77,123 @@ class JustETF(TickerBase):
         description = soup.find("div", {"id": "etf-description"}).text
 
         return description
+
+    # @property
+    # def etf_quote(self) -> pd.DataFrame:
+    #     """
+    #     Get the quote of the ETF
+
+    #     columns may include:
+    #     - Currency
+    #     - Price
+
+    #     Args:
+    #     ----------------
+    #     isin (str): ISIN of the ETF
+
+    #     Returns:
+    #     ----------------
+    #     pd.DataFrame: DataFrame containing the quote of the ETF
+    #     """
+    #     url = f"{config.JUSTETF_BASE_URL}/etf-profile.html?isin={self.isin}"
+
+    #     # build selenium interface object if not already built
+    #     if not hasattr(self, "selenium_interface"):
+    #         self.selenium_interface = selenium_interface()
+
+    #     soup = self.selenium_interface.get_html_content(url)
+
+    #     data_df = pd.DataFrame()
+
+    #     quote = soup.find("div", {"id": "realtime-quotes"})
+    #     currency, price = tuple(
+    #         quote.find("div", {"class": "col-xs-7"})
+    #         .find("div", {"class": "val"})
+    #         .text.split(" ")
+    #     )
+
+    #     data_df["currency"] = [currency]
+    #     data_df["price"] = [price]
+
+    @property
+    def etf_basics(self) -> pd.DataFrame:
+        """
+        Get the baisc information of the ETF
+
+        columns may include:
+        - Fund size
+        - Fund domicile
+        - Legal structure
+        - Replication
+
+        Args:
+        ----------------
+        isin (str): ISIN of the ETF
+
+        Returns:
+        ----------------
+        pd.DataFrame: DataFrame containing the basic information of the ETF
+        """
+        url = f"{config.JUSTETF_BASE_URL}/etf-profile.html?isin={self.isin}#basics"
+
+        # build selenium interface object if not already built
+        if not hasattr(self, "selenium_interface"):
+            self.selenium_interface = selenium_interface()
+
+        soup = self.selenium_interface.get_html_content(url)
+
+        data_df = pd.DataFrame()
+
+        table = soup.find("table", {"class": "table etf-data-table"})
+
+        for row in table.find_all("tr"):
+            column = row.find_all("td")
+            data_df[column[0].text.strip()] = [column[1].text.strip()]
+
+        return data_df
+
+    @property
+    def etf_holdings_companies(self) -> pd.DataFrame:
+        """
+        Get the top 10 holdings of the ETF by companies
+
+        columns may include:
+        - company name
+        - shares in percent
+
+        Args:
+        ----------------
+        isin (str): ISIN of the ETF
+
+        Returns:
+        ----------------
+        pd.DataFrame: DataFrame containing the holdings of the ETF
+        """
+        url = f"{config.JUSTETF_BASE_URL}/etf-profile.html?isin={self.isin}#holdings"
+
+        # build selenium interface object if not already built
+        if not hasattr(self, "selenium_interface"):
+            self.selenium_interface = selenium_interface()
+
+        soup = self.selenium_interface.get_html_content(url)
+
+        data_df = pd.DataFrame()
+        companies = []
+        shares_percent = []
+
+        table_body = (
+            soup.find(lambda tag: tag.name == "h3" and "Top 10 Holdings" in tag.text)
+            .find_next("table")
+            .find("tbody")
+        )
+
+        for row in table_body.find_all("tr"):
+            columns = row.find_all("td")
+            companies.append(columns[0].text)
+            shares_percent.append(columns[1].text)
+
+        data_df["company name"] = companies
+        data_df["shares in percent"] = shares_percent
+        data_df.set_index("company name", inplace=True)
+
+        return data_df
