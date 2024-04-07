a6b707de7523b43b979464ffd5e43238cdd3cf94
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-04-07 11:00:42 +0200

    Add tests for additional ETFs

--- tests/test_justetf.py
+++ tests/test_justetf.py
@@ -24,12 +24,16 @@ def test_general_info(isin: str) -> None:
     assert isinstance(general_info, pd.DataFrame)
     assert general_info.shape[0] == 1
     assert general_info.shape[1] >= 5
+    assert general_info.iloc[0]["TER"] != ""
+    for i in range(1, general_info.shape[1]):
+        assert general_info.iloc[0][i] != ""
 
 
 @pytest.mark.parametrize(
     "isin, expected",
     [
         ("IE00B4L5Y983", "A0RPWH"),
+        ("IE00B53SZB19", "A0YEDL"),
     ],
 )
 def test_wkn(isin: str, expected: str) -> None:
@@ -55,6 +59,7 @@ def test_no_isin() -> None:
     "isin",
     [
         ("IE00B4L5Y983"),
+        ("IE00B53SZB19"),
     ],
 )
 def test_description(isin: str) -> None:
@@ -66,3 +71,69 @@ def test_description(isin: str) -> None:
     description = etf.description
     assert isinstance(description, str)
     assert len(description) > 0
+
+
+# @pytest.mark.parametrize(
+#     "isin",
+#     [
+#         ("IE00B4L5Y983"),
+#         ("IE00B53SZB19"),
+#     ],
+# )
+# def test_etf_quote(isin: str) -> None:
+#     """
+#     Test the quote property of the JustETF class
+#     """
+#     etf = Ticker(isin=isin, security_type="etf")
+
+#     quote = etf.etf_quote
+#     assert isinstance(quote, pd.DataFrame)
+
+
+@pytest.mark.parametrize(
+    "isin",
+    [
+        ("IE00B4L5Y983"),
+        ("IE00B53SZB19"),
+    ],
+)
+def test_etf_basics(isin: str) -> None:
+    """
+    Test the etf_basics property of the JustETF class
+    """
+    etf = Ticker(isin=isin, security_type="etf")
+
+    etf_basics = etf.etf_basics
+    assert isinstance(etf_basics, pd.DataFrame)
+    assert etf_basics.shape[0] == 1
+    assert etf_basics.shape[1] >= 5
+
+    expceted_columns = [
+        "Fund size",
+        "Fund domicile",
+        "Legal structure",
+        "Replication",
+    ]
+
+    for column in expceted_columns:
+        assert column in etf_basics.columns
+        assert etf_basics[column].iloc[0] != ""
+
+
+@pytest.mark.parametrize(
+    "isin",
+    [
+        ("IE00B4L5Y983"),
+        ("IE00B53SZB19"),
+    ],
+)
+def test_etf_holdings_companies(isin: str) -> None:
+    """
+    Test the etf_holdings property of the JustETF class
+    """
+    etf = Ticker(isin=isin, security_type="etf")
+
+    etf_holdings = etf.etf_holdings_companies
+    assert isinstance(etf_holdings, pd.DataFrame)
+    assert etf_holdings.shape[0] == 10
+    assert etf_holdings.shape[1] == 1
