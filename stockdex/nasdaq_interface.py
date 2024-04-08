ebbc6af037c341b553eca1a1243344f9908cce4a
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-04-08 07:10:59 +0200

    Add methods for retrieving yearly and quarterly earnings forecast data

--- stockdex/nasdaq_interface.py
+++ stockdex/nasdaq_interface.py
@@ -57,3 +57,91 @@ class NASDAQInterface:
             data.append(row_data)
 
         return pd.DataFrame(data, columns=columns)
+
+    def yearly_earnings_forecast(self) -> pd.DataFrame:
+        """
+        Get yearly earnings for the stock
+
+        Returns:
+        ----------------
+        pd.DataFrame: Yearly earnings data
+        The columns might include:
+        - Fiscal Year End
+        - Consensus EPS* Forecast
+        - High EPS* Forecast
+        - Low EPS* Forecast
+        - Number Of Estimates
+        - Over The Last 4 Weeks Number Of Revisions - Up
+        - Over The Last 4 Weeks Number Of Revisions - Down
+        """
+
+        url = f"{self.base_url}/{self.ticker.lower()}/earnings"
+
+        # build selenium interface object if not already built
+        if not hasattr(self, "selenium_interface"):
+            self.selenium_interface = selenium_interface(use_custom_user_agent=True)
+
+        soup = self.selenium_interface.get_html_content(url)
+        earnings_table = soup.find("table", {"class": "earnings-forecast__table"})
+
+        columns = earnings_table.find(
+            "tr", {"class": "earnings-forecast__header"}
+        ).find_all("th")
+        columns = [column.text for column in columns]
+
+        data = []
+        table_body = earnings_table.find(
+            "tbody", {"class": "earnings-forecast__table-body"}
+        )
+        for row in table_body.find_all("tr"):
+            row_data_th = [cell.text for cell in row.find_all("th")]
+            row_data_td = [cell.text for cell in row.find_all("td")]
+            row_data = row_data_th + row_data_td
+            data.append(row_data)
+
+        return pd.DataFrame(data, columns=columns)
+
+    def quarterly_earnings_forecast(self) -> pd.DataFrame:
+        """
+        Get quarterly earnings forecast for the stock
+
+        Returns:
+        ----------------
+        pd.DataFrame: Quarterly earnings forecast data
+        The columns might include:
+        - Fiscal Quarter End
+        - Consensus EPS* Forecast
+        - High EPS* Forecast
+        - Low EPS* Forecast
+        - Number Of Estimates
+        - Over The Last 4 Weeks Number Of Revisions - Up
+        - Over The Last 4 Weeks Number Of Revisions - Down
+        """
+
+        url = f"{self.base_url}/{self.ticker.lower()}/earnings"
+
+        # build selenium interface object if not already built
+        if not hasattr(self, "selenium_interface"):
+            self.selenium_interface = selenium_interface(use_custom_user_agent=True)
+
+        soup = self.selenium_interface.get_html_content(url)
+        earnings_table = soup.find_all("table", {"class": "earnings-forecast__table"})[
+            1
+        ]
+
+        columns = earnings_table.find(
+            "tr", {"class": "earnings-forecast__header"}
+        ).find_all("th")
+        columns = [column.text for column in columns]
+
+        data = []
+        table_body = earnings_table.find(
+            "tbody", {"class": "earnings-forecast__table-body"}
+        )
+        for row in table_body.find_all("tr"):
+            row_data_th = [cell.text for cell in row.find_all("th")]
+            row_data_td = [cell.text for cell in row.find_all("td")]
+            row_data = row_data_th + row_data_td
+            data.append(row_data)
+
+        return pd.DataFrame(data, columns=columns)
