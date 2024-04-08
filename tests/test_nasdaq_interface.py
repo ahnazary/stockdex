1e8a0f3acb913998fa3bd776c49252b1d1ee3da4
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-04-08 07:11:03 +0200

    Add tests for yearly and quarterly earnings forecast

--- tests/test_nasdaq_interface.py
+++ tests/test_nasdaq_interface.py
@@ -34,3 +34,61 @@ def test_quarterly_earnings_surprise(ticker):
         "% Surprise",
     ]
     assert response.columns.tolist() == expected_columns
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("NVDA"),
+        ("AAPL"),
+        ("GOOGL"),
+        ("MSFT"),
+    ],
+)
+def test_yearly_earnings_forecast(ticker):
+    nasdaq_interface = NASDAQInterface(ticker)
+    response = nasdaq_interface.yearly_earnings_forecast()
+
+    assert response is not None
+    assert isinstance(response, pd.DataFrame)
+    assert response.shape[0] > 1
+    assert response.shape[1] > 1
+
+    expected_columns = [
+        "Fiscal Year End",
+        "Consensus EPS* Forecast",
+        "High EPS* Forecast",
+        "Low EPS* Forecast",
+    ]
+
+    for column in expected_columns:
+        assert column in response.columns.tolist()
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("NVDA"),
+        ("AAPL"),
+        ("GOOGL"),
+        ("MSFT"),
+    ],
+)
+def test_quarterly_earnings_forecast(ticker):
+    nasdaq_interface = NASDAQInterface(ticker)
+    response = nasdaq_interface.quarterly_earnings_forecast()
+
+    assert response is not None
+    assert isinstance(response, pd.DataFrame)
+    assert response.shape[0] > 1
+    assert response.shape[1] > 1
+
+    expected_columns = [
+        "Fiscal Quarter End",
+        "Consensus EPS* Forecast",
+        "High EPS* Forecast",
+        "Low EPS* Forecast",
+    ]
+
+    for column in expected_columns:
+        assert column in response.columns.tolist()
