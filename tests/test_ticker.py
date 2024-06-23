d4728771b423ad5ed1938e33a42edb314455d6a4
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-06-23 22:18:43 +0200

    Refactor Ticker class and remove TickerFactory class

--- tests/test_ticker.py
+++ tests/test_ticker.py
@@ -1,7 +1,6 @@
 import pytest
 
-from stockdex.exceptions import WrongDataSource
-from stockdex.ticker import TickerFactory
+from stockdex.ticker import Ticker
 
 
 @pytest.mark.parametrize(
@@ -14,19 +13,10 @@ from stockdex.ticker import TickerFactory
 )
 def test_get_response(ticker, expected_response):
     # Create a Ticker object
-    ticker = TickerFactory(ticker=ticker).ticker
+    ticker = Ticker(ticker=ticker)
 
     # Send an HTTP GET request to the website
     response = ticker.get_response(f"https://finance.yahoo.com/quote/{ticker.ticker}")
 
     # Check if the response is as expected
     assert response.status_code == expected_response
-
-
-def test_data_source_invalid():
-    # Create a Ticker object
-    ticker = TickerFactory(ticker="AAPL")
-
-    # Check if the exception is raised
-    with pytest.raises(WrongDataSource):
-        ticker.data_source = "invalid_data_source"
