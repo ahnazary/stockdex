64058be41b0a8a8bc14ebc8f0a7b0194d48722b1
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-06-24 15:19:28 +0200

    refactor: Update class method names to follow naming convention

--- stockdex/digrin_interface.py
+++ stockdex/digrin_interface.py
@@ -41,7 +41,7 @@ class Digrin_Interface(TickerBase):
         soup = BeautifulSoup(response.content, "html.parser")
 
         try:
-            table = soup.find_all("table")[0]
+            table = self.find_parent_by_text(soup, "table", "Ex-dividend date")
         except IndexError:
             raise Exception(f"There is no dividend data for the ticker {self.ticker}")
 
@@ -73,11 +73,43 @@ class Digrin_Interface(TickerBase):
         soup = BeautifulSoup(response.content, "html.parser")
 
         try:
-            table = soup.find("table")
+            table = self.find_parent_by_text(soup, "table", "Payout ratio")
         except IndexError:
             raise Exception(
                 f"There is no payout ratio data for the ticker {self.ticker}"
             )
+
+        data_df = pd.DataFrame()
+        data = []
+
+        headers = [th.text for th in table.find_all("thead")[0].find_all("th")]
+        for tr in table.find_all("tbody")[0].find_all("tr"):
+            data.append([td.text for td in tr.find_all("td")])
+
+        data_df = pd.DataFrame(data, columns=headers)
+        return data_df
+
+    @property
+    def digrin_price(self) -> pd.DataFrame:
+        """
+        Get price for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the price
+        visible in the digrin website for the ticker
+        """
+
+        # URL of the website to scrape
+        url = f"{DIGRIN_BASE_URL}/{self.ticker}/price"
+        response = self.get_response(url)
+
+        # Parse the HTML content of the website
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        try:
+            table = self.find_parent_by_text(soup, "table", "Adjusted price")
+        except IndexError:
+            raise Exception(f"There is no price data for the ticker {self.ticker}")
 
         data_df = pd.DataFrame()
         data = []
@@ -107,7 +139,7 @@ class Digrin_Interface(TickerBase):
         soup = BeautifulSoup(response.content, "html.parser")
 
         try:
-            table = soup.find("table")
+            table = self.find_parent_by_text(soup, "table", "Split Ratio")
         except IndexError:
             raise Exception(
                 f"There is no stock split data for the ticker {self.ticker}"
@@ -122,3 +154,191 @@ class Digrin_Interface(TickerBase):
 
         data_df = pd.DataFrame(data, columns=headers)
         return data_df
+
+    def _get_table_from_url(self, keyword: str, url: str) -> pd.DataFrame:
+        """
+        Get the table from the financials page for the ticker
+
+        Args:
+        keyword (str): The keyword to search for in the table
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the table
+        visible in the digrin website for the ticker
+        """
+
+        # URL of the website to scrape
+        url = url
+        response = self.get_response(url)
+
+        # Parse the HTML content of the website
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        try:
+            table = self.find_parent_by_text(soup, "table", keyword)
+        except IndexError:
+            raise Exception(f"There is no {keyword} data for the ticker {self.ticker}")
+
+        data_df = pd.DataFrame()
+        data = []
+
+        headers = [th.text for th in table.find_all("thead")[0].find_all("th")]
+        for tr in table.find_all("tbody")[0].find_all("tr"):
+            data.append([td.text for td in tr.find_all("td")])
+
+        data_df = pd.DataFrame(data, columns=headers)
+        return data_df
+
+    @property
+    def digrin_assets_vs_liabilities(self) -> pd.DataFrame:
+        """
+        Get assets vs liabilities for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the assets vs liabilities
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Assets", f"{DIGRIN_BASE_URL}/{self.ticker}/financials"
+        )
+
+    @property
+    def digrin_free_cash_flow(self) -> pd.DataFrame:
+        """
+        Get free cash flow for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the free cash flow
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Free Cash Flow", f"{DIGRIN_BASE_URL}/{self.ticker}/financials"
+        )
+
+    @property
+    def digrin_net_income(self) -> pd.DataFrame:
+        """
+        Get net income for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the net income
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Net Income", f"{DIGRIN_BASE_URL}/{self.ticker}/financials"
+        )
+
+    @property
+    def digrin_cash_and_debt(self) -> pd.DataFrame:
+        """
+        Get cash and debt for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the cash and debt
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Capital Lease", f"{DIGRIN_BASE_URL}/{self.ticker}/financials"
+        )
+
+    @property
+    def digrin_shares_outstanding(self) -> pd.DataFrame:
+        """
+        Get shares outstanding for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the shares outstanding
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Shares Outstanding", f"{DIGRIN_BASE_URL}/{self.ticker}/financials"
+        )
+
+    @property
+    def digrin_expenses(self) -> pd.DataFrame:
+        """
+        Get expenses for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the expenses
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Capex", f"{DIGRIN_BASE_URL}/{self.ticker}/financials"
+        )
+
+    @property
+    def digrin_cost_of_revenue(self) -> pd.DataFrame:
+        """
+        Get cost of revenue for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the cost of revenue
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Cost of Revenue", f"{DIGRIN_BASE_URL}/{self.ticker}/financials"
+        )
+
+    @property
+    def digrin_dgr3(self) -> pd.DataFrame:
+        """
+        Get dgr3 for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the dgr3
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Estimated Yield on Cost", f"{DIGRIN_BASE_URL}/{self.ticker}/dgr3"
+        )
+
+    @property
+    def digrin_dgr5(self) -> pd.DataFrame:
+        """
+        Get dgr5 for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the dgr5
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Estimated Yield on Cost", f"{DIGRIN_BASE_URL}/{self.ticker}/dgr5"
+        )
+
+    @property
+    def digrin_dgr10(self) -> pd.DataFrame:
+        """
+        Get dgr10 for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the dgr10
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Estimated Yield on Cost", f"{DIGRIN_BASE_URL}/{self.ticker}/dgr10"
+        )
+
+    @property
+    def digrin_upcoming_estimated_earnings(self) -> pd.DataFrame:
+        """
+        Get upcoming estimated earnings for the ticker
+
+        Returns:
+        pd.DataFrame: A pandas DataFrame including the upcoming estimated earnings
+        visible in the digrin website for the ticker
+        """
+
+        return self._get_table_from_url(
+            "Actual / Estimated EPS", f"{DIGRIN_BASE_URL}/{self.ticker}/earnings"
+        )
--- tests/test_digrin_interface.py
+++ tests/test_digrin_interface.py
@@ -65,3 +65,265 @@ def test_digrin_stock_splits(ticker):
     assert isinstance(digrin_stock_splits, pd.DataFrame)
     assert digrin_stock_splits.shape[0] > 1
     assert digrin_stock_splits.shape[1] == 2
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_price(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_price = ticker.digrin_price
+
+    # Check if the response is as expected
+    assert isinstance(digrin_price, pd.DataFrame)
+    assert digrin_price.shape[0] > 1
+    assert digrin_price.shape[1] == 3
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_assets_vs_liabilities(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_assets_vs_liabilities = ticker.digrin_assets_vs_liabilities
+
+    # Check if the response is as expected
+    assert isinstance(digrin_assets_vs_liabilities, pd.DataFrame)
+    assert digrin_assets_vs_liabilities.shape[0] > 1
+    assert digrin_assets_vs_liabilities.shape[1] == 3
+    assert "Assets" in digrin_assets_vs_liabilities.columns
+    assert "Liabilities" in digrin_assets_vs_liabilities.columns
+    assert "Date" in digrin_assets_vs_liabilities.columns
+    assert digrin_assets_vs_liabilities.iloc[0]["Assets"] != ""
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_free_cash_flow(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_free_cash_flow = ticker.digrin_free_cash_flow
+
+    # Check if the response is as expected
+    assert isinstance(digrin_free_cash_flow, pd.DataFrame)
+    assert digrin_free_cash_flow.shape[0] > 1
+    assert digrin_free_cash_flow.shape[1] == 3
+    assert "Free Cash Flow" in digrin_free_cash_flow.columns
+    assert "Date" in digrin_free_cash_flow.columns
+    assert digrin_free_cash_flow.iloc[0]["Free Cash Flow"] != ""
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_net_income(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_net_income = ticker.digrin_net_income
+
+    # Check if the response is as expected
+    assert isinstance(digrin_net_income, pd.DataFrame)
+    assert digrin_net_income.shape[0] > 1
+    assert digrin_net_income.shape[1] == 2
+    assert "Net Income" in digrin_net_income.columns
+    assert "Date" in digrin_net_income.columns
+    assert digrin_net_income.iloc[0]["Net Income"] != ""
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_cash_and_debt(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_cash_and_debt = ticker.digrin_cash_and_debt
+
+    # Check if the response is as expected
+    assert isinstance(digrin_cash_and_debt, pd.DataFrame)
+    assert digrin_cash_and_debt.shape[0] > 1
+    assert digrin_cash_and_debt.shape[1] == 4
+    assert "Cash" in digrin_cash_and_debt.columns
+    assert "Debt" in digrin_cash_and_debt.columns
+    assert "Date" in digrin_cash_and_debt.columns
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_shares_outstanding(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_shares_outstanding = ticker.digrin_shares_outstanding
+
+    # Check if the response is as expected
+    assert isinstance(digrin_shares_outstanding, pd.DataFrame)
+    assert digrin_shares_outstanding.shape[0] > 1
+    assert digrin_shares_outstanding.shape[1] == 2
+    assert "Shares Outstanding" in digrin_shares_outstanding.columns
+    assert "Date" in digrin_shares_outstanding.columns
+    assert digrin_shares_outstanding.iloc[0]["Shares Outstanding"] != ""
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_expenses(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_expenses = ticker.digrin_expenses
+
+    # Check if the response is as expected
+    assert isinstance(digrin_expenses, pd.DataFrame)
+    assert digrin_expenses.shape[0] > 1
+    assert digrin_expenses.shape[1] == 5
+    assert "Date" in digrin_expenses.columns
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_cost_of_revenue(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_cost_of_revenue = ticker.digrin_cost_of_revenue
+
+    # Check if the response is as expected
+    assert isinstance(digrin_cost_of_revenue, pd.DataFrame)
+    assert digrin_cost_of_revenue.shape[0] > 1
+    assert digrin_cost_of_revenue.shape[1] == 3
+    assert "Date" in digrin_cost_of_revenue.columns
+    assert "Cost of Revenue" in digrin_cost_of_revenue.columns
+    assert digrin_cost_of_revenue.iloc[0]["Cost of Revenue"] != ""
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_dgr3(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_dgr3 = ticker.digrin_dgr3
+
+    # Check if the response is as expected
+    assert isinstance(digrin_dgr3, pd.DataFrame)
+    assert digrin_dgr3.shape[0] > 1
+    assert digrin_dgr3.shape[1] == 3
+    assert "Year" in digrin_dgr3.columns
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_dgr5(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_dgr5 = ticker.digrin_dgr5
+
+    # Check if the response is as expected
+    assert isinstance(digrin_dgr5, pd.DataFrame)
+    assert digrin_dgr5.shape[0] > 1
+    assert digrin_dgr5.shape[1] == 3
+    assert "Year" in digrin_dgr5.columns
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("MSFT"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_dgr10(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_dgr10 = ticker.digrin_dgr10
+
+    # Check if the response is as expected
+    assert isinstance(digrin_dgr10, pd.DataFrame)
+    assert digrin_dgr10.shape[0] > 1
+    assert digrin_dgr10.shape[1] == 3
+    assert "Year" in digrin_dgr10.columns
+
+
+@pytest.mark.parametrize(
+    "ticker",
+    [
+        ("AAPL"),
+        ("ASML"),
+        ("CAT"),
+        ("BAC"),
+    ],
+)
+def test_digrin_upcoming_estimated_earnings(ticker):
+    ticker = Ticker(ticker=ticker)
+    digrin_upcoming_estimated_earnings = ticker.digrin_upcoming_estimated_earnings
+
+    # Check if the response is as expected
+    assert isinstance(digrin_upcoming_estimated_earnings, pd.DataFrame)
+    assert digrin_upcoming_estimated_earnings.shape[0] >= 1
+    assert digrin_upcoming_estimated_earnings.shape[1] == 5
+    assert "Date" in digrin_upcoming_estimated_earnings.columns
