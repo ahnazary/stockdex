cefa65563b0289d676b28167408e9f7fbbdedda7
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-10-24 20:35:55 +0200

    Add dash test

---
+++ tests/test_lib.py
@@ -0,0 +1,41 @@
+"""
+Test the lib module
+"""
+import os
+import threading
+import time
+
+import pytest
+
+from stockdex.lib import plot_multiple_categories
+from stockdex.ticker import Ticker
+
+skip_test = bool(os.getenv("SKIP_TEST", False))
+
+
+@pytest.mark.skipif(skip_test, reason="Skipping in GH action as it is visual")
+@pytest.mark.parametrize(
+    "ticker",
+    [("AAPL"), ("BAC"), ("CAT"), ("ASML"), ("MSFT"), ("PLTR"), ("DHL.DE")],
+)
+def test_complex_digrin_plot(ticker):
+    """
+    Test that builds multiple figures and plots them together in one dashboard
+    """
+
+    ticker = Ticker(ticker=ticker)
+    fig_1 = ticker.plot_digrin_expenses(show_plot=False)
+    fig_2 = ticker.plot_digrin_cost_of_revenue(show_plot=False)
+
+    # Function to run the Dash app
+    def run_dash_app():
+        plot_multiple_categories(ticker=ticker.ticker, figures=[fig_1, fig_2])
+
+    # Start the Dash app in a separate thread
+    dash_thread = threading.Thread(target=run_dash_app)
+    dash_thread.daemon = True
+    dash_thread.start()
+
+    # Allow the Dash app to run for a short duration (e.g., 5 seconds)
+    print("Waiting for Dash app to run for 30 seconds")
+    time.sleep(30)
