bb41662888dd0aaf1f2f00d52199220d3af6787a
ahnazary <ah.nazary.aghchemazary@gmail.com>
2024-10-24 20:35:45 +0200

    Add dash app implementataion

--- stockdex/lib.py
+++ stockdex/lib.py
@@ -1,8 +1,10 @@
 import platform
-from typing import Union
+from typing import List, Union
 
+import dash
 import pandas as pd
 import plotly.express as px
+from dash import dcc, html
 
 from stockdex.exceptions import WrongSecurityType
 
@@ -40,6 +42,7 @@ def plot_dataframe(
     y_axis_title: str = "Amount",
     x_axis_title: str = "Date",
     draw_line_chart: bool = False,
+    show_plot: bool = True,
 ) -> None:
     """
     Plot a DataFrame using Plotly Express
@@ -63,7 +66,25 @@ def plot_dataframe(
     template : str
         The template of the plot.
         One of plotly, plotly_white, plotly_dark, ggplot2, seaborn, simple_white, none
-        default: "plotly"
+        default: "plotly"#
+
+    y_axis_title : str
+        The title of the y-axis
+        default: "Amount"
+
+    x_axis_title : str
+        The title of the x-axis
+        default: "Date"
+
+    draw_line_chart : bool
+        If the plot should be a line chart and not bar. For charts that have too many data points,
+        it is recommended to use line charts instead of bar charts as it is easier to read.
+        default: False (bar chart)
+
+    show_plot : bool
+        If the plot should be shown. Setting this to False will only return the plot object.
+        Can be used to save the plot to a file or construct complex dashboards with multiple plots.
+        default: True
     """
     if draw_line_chart:
         fig = px.line(
@@ -89,4 +110,55 @@ def plot_dataframe(
     # make bar width wider
     fig.update_traces(marker_line_width=1.5)
 
-    fig.show()
+    if show_plot:
+        fig.show()
+
+    return fig
+
+
+def plot_multiple_categories(
+    ticker: str,
+    figures: List[Union[px.bar, px.line]],
+    app_port: int = 8050,
+) -> None:
+    """
+    Plot multiple categories in a single Dash app.
+
+    Parameters
+    ----------
+    figures : List[Union[px.bar, px.line]]
+        A list of Plotly Express bar or line objects
+
+    app_port : int
+        The port to run the Dash app on
+        default: 8050
+    """
+
+    # Initialize the Dash app
+    app = dash.Dash(__name__)
+
+    # Create a list of Graph components, one for each figure in the list
+    graphs = []
+    for i, fig in enumerate(figures):
+        graphs.append(
+            html.Div(
+                dcc.Graph(id=f"figure-{i}", figure=fig),
+                style={"width": "48%", "display": "inline-block", "padding": "10px"},
+            )
+        )
+
+    # Define the layout of the app
+    app.layout = html.Div(
+        children=[
+            html.H1(
+                children=f"Multiple Category Dashboard for {ticker}",
+                style={"text-align": "center"},
+            ),
+            # Create a container for the list of figures
+            html.Div(children=graphs),
+        ]
+    )
+
+    # Start Dash app in a separate threa
+    print(f"Starting Dash app on port {app_port}")
+    app.run_server(debug=True, use_reloader=False, port=app_port)
